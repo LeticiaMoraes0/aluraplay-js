40348f8cd96bb1ba35474e03f6bd5211d8624e2b
Leticia1508 <leticiademoraes0@gmail.com>
2024-02-03 20:51:27 -0300

    feat: Buscando elementos

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,27 +5,34 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
-async function criaVideo(titulo, descricao, url, imagem )  {
+async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
-        method: "POST" ,
+        method: "POST",
         headers: {
             "Content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
-            descricao: `${descricao} mil visualizaçoes`,
+            descricao: `${descricao} mil visualizações`,
             url: url,
             imagem: imagem
-
         })
     });
 
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
     const conexaoConvertida = await conexao.json();
 
-        return conexaoConvertida;
- }
+    return conexaoConvertida;
+}
 
 export const conectaApi = {
     listaVideos,
     criaVideo,
+    buscaVideo
 }
